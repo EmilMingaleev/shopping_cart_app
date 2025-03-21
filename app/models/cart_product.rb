ca4d1d28229fef510e58960a5e9003ed072e5bff
EmilMingaleev <mingaleev_emil@mail.ru>
2025-03-21 17:09:55 +0300

    lint fix

--- app/models/cart_product.rb
+++ app/models/cart_product.rb
@@ -2,7 +2,6 @@ class CartProduct < ApplicationRecord
   belongs_to :cart
   belongs_to :product
 
-  # Метод для расчета общей стоимости товара в корзине
   def total_price
     quantity * product.price
   end
