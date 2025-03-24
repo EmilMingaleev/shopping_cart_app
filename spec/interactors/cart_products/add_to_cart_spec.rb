b83fe125e45a41c0afb82bac3d8110be6b31e282
EmilMingaleev <mingaleev_emil@mail.ru>
2025-03-24 13:49:16 +0300

    spec fix

--- spec/interactors/cart_products/add_to_cart_spec.rb
+++ spec/interactors/cart_products/add_to_cart_spec.rb
@@ -15,19 +15,19 @@ RSpec.describe CartProducts::AddToCart, type: :interactor do
     it 'when cart is not found' do
       result = described_class.call(cart: nil, product: product, quantity: 2)
       expect(result).to be_failure
-      expect(result.error).to eq('Cart not found')
+      expect(result.error).to eq('Корзина не найдена')
     end
 
     it 'when product is not found' do
       result = described_class.call(cart: cart, product: nil, quantity: 2)
       expect(result).to be_failure
-      expect(result.error).to eq('Product not found')
+      expect(result.error).to eq('Товар не найден')
     end
 
     it 'when quantity is less than 1' do
       result = described_class.call(cart: cart, product: product, quantity: 0)
       expect(result).to be_failure
-      expect(result.error).to eq('Quantity must be greater than 0')
+      expect(result.error).to eq('Количество должно быть больше 0')
     end
   end
 end
